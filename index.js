60d193361d891c9fafb1637f053e397dcd34dacf
Mohit Sapru <mohit.sapru@rollick.io>
2022-11-23 01:01:17 +0530

    Upgrade winston package

--- index.js
+++ index.js
@@ -13,24 +13,60 @@ var util = require('util'),
         "white": "fff",
         "gray": "808080",
         "grey": "808080"
-    },
+    };
+const Transport = require('winston-transport');
 
-    Office365ConnectorHook = winston.transports.Office365ConnectorHook = function (options) {
+class Office365ConnectorHook extends Transport {
+    constructor(options) {
+        super(options);
         this.name = options.name || 'office365connectorhook';
         this.level = options.level || 'silly';
-
         this.hookUrl = options.hookUrl || null;
-
         this.webColors = options.colors || {};
-
         this.prependLevel = options.prependLevel === undefined ? true : options.prependLevel;
         this.appendMeta = options.appendMeta === undefined ? true : options.appendMeta;
-
         this.formatter = options.formatter || null;
-    },
-    q = queue({ concurrency: 1, autostart: true });
+    }
 
-util.inherits(Office365ConnectorHook, winston.Transport);
+    log(info, callback) {
+        let color = winston.config.syslog.colors[info.level],
+            themeColor = this.webColors[info.level] || webColors[color],
+            title = "";
+        let message = '';
+        if (this.prependLevel) {
+            message += '[' + info.level + ']\n';
+        }
+        message += info.message;
+
+        if (this.appendMeta && info.meta) {
+            title = info.meta.title || "";
+            delete info.meta["title"];
+            var props = Object.getOwnPropertyNames(info.meta);
+            if (props.length) {
+                // http://stackoverflow.com/questions/18391212/is-it-not-possible-to-stringify-an-error-using-json-stringify#comment57014279_26199752
+                message += ' ```' + JSON.stringify(info.meta, props, 2) + '```';
+            }
+        }
+
+        if (typeof this.formatter === 'function') {
+            message = this.formatter({
+                level: info.level,
+                message: message,
+                meta: info.meta
+            });
+        }
+
+        let payload = {
+            title: title,
+            text: message,
+            themeColor: themeColor
+        };
+
+        q.push(deferredExecute(this.hookUrl, payload, callback));
+    }
+}
+
+var q = queue({ concurrency: 1, autostart: true });
 
 ensureCallback = function (cb1, cb2) {
     return function () {
@@ -62,45 +98,5 @@ deferredExecute = function (url, payload, callback) {
             });
     }
 }
-
-Office365ConnectorHook.prototype.log = function (level, msg, meta, callback) {
-    var color = winston.config.allColors[level],
-        themeColor = this.webColors[level] || webColors[color],
-        title = "";
-
-    var message = '';
-
-    if (this.prependLevel) {
-        message += '[' + level + ']\n';
-    }
-
-    message += msg;
-
-    if (this.appendMeta && meta) {
-        title = meta.title || "";
-        delete meta["title"];
-        var props = Object.getOwnPropertyNames(meta);
-        if (props.length) {
-            // http://stackoverflow.com/questions/18391212/is-it-not-possible-to-stringify-an-error-using-json-stringify#comment57014279_26199752
-            message += ' ```' + JSON.stringify(meta, props, 2) + '```';
-        }
-    }
-
-    if (typeof this.formatter === 'function') {
-        message = this.formatter({
-            level: level,
-            message: message,
-            meta: meta
-        });
-    }
-
-    var payload = {
-        title: title,
-        text: message,
-        themeColor: themeColor
-    };
-
-    q.push(deferredExecute(this.hookUrl, payload, callback));
-};
 
 module.exports = Office365ConnectorHook;
--- test.js
+++ test.js
@@ -1,15 +1,14 @@
 var Office365ConnectorHook = require("./index.js"),
     winston = require('winston'),
-    Logger = winston.Logger,
+    //Logger = winston.Logger,
     Console = winston.transports.Console,
     hookUrl = process.env.HOOK_URL;
-
 if (!hookUrl) {
     console.warn("No process.env.HOOK_URL set. Please set it to your Connector Webhook URL before running this test.");
     process.exit();
 }
 
-var logger = new Logger({
+var logger = winston.createLogger({
     transports: [
         new Console({}),
         new Office365ConnectorHook({
